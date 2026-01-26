2650263475c43f724e465915ed55e524ecad6f8a
Rik Froeling <rik.froeling@wur.nl>
2026-01-26 16:06:16 +0100

    Add saving to .h5 functionality

--- clean_edges.py
+++ clean_edges.py
@@ -28,6 +28,30 @@ def load_h5_dataset(path: Path, key: str) -> np.ndarray:
     return dataset
 
 
+def save_h5_dataset(path: Path, stack: np.ndarray, key: str, mode: str = "a") -> None:
+    """
+    Create a dataset inside a h5 file from a numpy array.
+
+    Args:
+        path (Path): path to the h5 file
+        stack (np.ndarray): numpy array to save as dataset in the h5 file.
+        key (str): key of the dataset in the h5 file.
+        mode (str): mode to open the h5 file ['w', 'a'].
+
+    """
+
+    if key is None:
+        raise ValueError("Key is required to create a dataset in a h5 file.")
+
+    if key == "":
+        raise ValueError("Key cannot be empty to create a dataset in a h5 file.")
+
+    with h5py.File(path, mode) as f:
+        if key in f:
+            del f[key]
+        f.create_dataset(key, data=stack, compression="gzip")
+
+
 def display_XY_slice(dataset: np.ndarray, LUT: str ='gray', show=True) -> None:
     z = dataset.shape[0] // 2
 
@@ -373,6 +397,7 @@ def main():
         print(f"Processing {h5_file.name}...")
         segmentation, cleaned_segmentation = cleanup_segmentation(h5_file, key)
         create_comparison_plot(segmentation, cleaned_segmentation, h5_file, save=True)
+        save_h5_dataset(h5_file, cleaned_segmentation, key='cleaned')
 
 
 if __name__ == "__main__":
