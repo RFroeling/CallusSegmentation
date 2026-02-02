0a270526be8cd5d9fe9a08b72a990baeab484ebc
Rik Froeling <rik.froeling@wur.nl>
2026-02-02 16:09:44 +0100

    Quick implementation to move .h5s after review

---
+++ src/move_files.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from pathlib import Path
+
+from segmentation.h5 import move_h5
+
+src_path = Path("W:\\AFSG\\Groups\\BIC\\Bulk\\Rik\\Experiments\\251201_CallusSegmentation\\stellaris\\plantseg_labels\\comparison_plots")
+dest_path = src_path.parents[1] / 'curated_labels'
+dest_path.mkdir(parents=True, exist_ok=True)
+log_path = src_path.parent / '_logs' / 'review_log.csv'
+
+df = pd.read_csv(log_path)
+
+accepted_files = df.loc[df['Decision'] == 'accepted', 'FileName'].tolist()
+
+for file in accepted_files:
+    file = file.replace('comparison_', "")
+    file = file.replace('.png', '.h5')
+    file_path = src_path.parent / file
+    move_h5(file_path, dest_path)
+
+print('Done')
