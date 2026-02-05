ec630724e870eab52321c9c04292b0d5b05e97df
Rik Froeling <rik.froeling@wur.nl>
2026-02-05 10:33:49 +0100

    Configure argparsing

--- segmentation/entry.py
+++ segmentation/entry.py
@@ -1,5 +1,83 @@
-from segmentation.tasks.review import ImageReviewer
+import argparse
+
+def create_parser():
+    """Create and return the argument parser for the CLI."""
+    arg_parser = argparse.ArgumentParser()
+    mode = arg_parser.add_mutually_exclusive_group(required=True)
+    mode.add_argument(
+        "--review",
+        "-r",
+        action='store_true',
+        help="Launch an instance of the Image Reviewer.",
+    )
+    mode.add_argument(
+        "--clean",
+        "-c",
+        action='store_true',
+        help="Cleans images (Specify Path and key in .env file.)",
+    )
+    mode.add_argument(
+        "--convert",
+        "-cv",
+        action='store_true',
+        help="Converts .lif images to OME-tiff (Specify Paths in .env file.)",
+    )
+    mode.add_argument(
+        "--inspect",
+        "-i",
+        action='store_true',
+        help="Inspects the contents of .h5 file, prints to console.",
+    )
+    # Add arguments here if neccesary
+    return arg_parser.parse_args()
+
+
+def launch_reviewer():
+    """Start the reviewer to review files."""
+    from segmentation.core.views import ImageReviewer
+
+    viewer = ImageReviewer()
+    viewer.run()
+
+
+def clean_edges():
+    """Cleans the boundary labels from segmented images"""
+    from segmentation.tasks.clean_edges import main
+
+    main()
+
+
+def convert_images():
+    """Convert .lif file(s) to .ome.tiff"""
+    from segmentation.tasks.convert_lif import main
+
+    main()
+
+
+def inspect_file():
+    """Inspect the content of user specified file(s)"""
+    from segmentation.tasks.inspect_h5 import main
+    
+    main()
+
 
 def main():
-    viewer =  ImageReviewer()
-    viewer.run()
+    """Main function to parse arguments and call corresponding functionality."""
+    args = create_parser()
+
+    if args.review:
+        launch_reviewer()
+    elif args.clean:
+        clean_edges()
+    elif args.convert:
+        convert_images()
+    elif args.inspect:
+        inspect_file()
+    else:
+        raise ValueError(
+            "Wrong arguments. Run `segmentation -h` to see the available options."
+        )
+
+
+if __name__ == "__main__":
+    main()
