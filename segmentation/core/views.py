a0fa3402468b857f452e72d3f0dbb5e31b5baafa
Rik Froeling <rik.froeling@wur.nl>
2026-02-06 12:02:21 +0100

    Deplot gitlab pages

--- segmentation/core/views.py
+++ segmentation/core/views.py
@@ -187,7 +187,7 @@ class ImageReviewer(tk.Tk):
     def set_icon(self):
         parent_dir = Path(__file__).parents[2]
         icon_path = parent_dir / "img" / "icon.png"
-        
+
         if icon_path.exists():
             self.iconphoto(False, tk.PhotoImage(file=icon_path))
 
